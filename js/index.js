23f98c28c0c07861eaef678007eb89018b9f51f0
chenxi-chenjia <709207209@qq.com>
2017-05-05 10:16:54 +0800

    up

---
+++ js/index.js
@@ -0,0 +1,9 @@
+var mySwiper = new Swiper('.swiper-container', {
+	autoplay: 0,//可选选项，自动滑动
+	noSwiping:false,
+	loop:true,
+	direction: 'vertical',
+	pagination: '.swiper-pagination',
+    paginationClickable: true,
+    mousewheelControl : true
+})
